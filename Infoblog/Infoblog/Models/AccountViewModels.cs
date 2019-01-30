4afa70637a96268bdd7c15c538ea667bd9c63685
martinloord <43519485+martinloord@users.noreply.github.com>
2019-01-30 14:20:42 +0100

    Test

--- Infoblog/Infoblog/Models/AccountViewModels.cs
+++ Infoblog/Infoblog/Models/AccountViewModels.cs
@@ -70,6 +70,14 @@ namespace Infoblog.Models
         public string Email { get; set; }
 
         [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "Invalid Firstname, use letters A-Z")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "Invalid Lastname, use letters A-Z")]
+        public string Lastname { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
