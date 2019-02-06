6be9c14e619ea47ddbe241829a79f14c1600bc9a
Henrik Ojeda <henrik.ojeda@hotmail.com>
2019-02-06 14:01:16 +0100

    Notification class- in progress

---
+++ Infoblog/Infoblog/Models/Notifications.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Infoblog.Models
+{
+    public class Notifications
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string ReciverID { get; set; }
+        public string Message { get; set; }
+
+    }
+}
