2be519bc1c30e5d4cb5499477a0ddcf64677adee
Simon <s@sfk.sef>
2019-02-07 13:21:08 +0100

    Lide oppdatering på viewvote

--- Infoblog/Infoblog/Models/IdentityModels.cs
+++ Infoblog/Infoblog/Models/IdentityModels.cs
@@ -42,7 +42,6 @@ namespace Infoblog.Models
 
         public DbSet<Meeting> Meetings { get; set; }
         public DbSet<MeetingPoll> MeetingPolls { get; set; }
-        public DbSet<CategoryModel> Category { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
