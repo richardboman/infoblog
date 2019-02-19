8170510f6ecde693e66b4619a864cff71972b1f4
Oskar <Oskar@DESKTOP-VJ9DCAD>
2019-02-19 10:53:30 +0100

    Validering

--- Infoblog/Infoblog/Scripts/validationScripts.js
+++ Infoblog/Infoblog/Scripts/validationScripts.js
@@ -3,6 +3,14 @@
         return value.trim() != "";
     }, "Inte bara mellanslag, tack!");
 
+    jQuery.validator.addMethod("notDefaultHour", function (value, element) {
+        return value.trim() != "Timmar";
+    }, "Vänligen välj en tid!");
+
+    jQuery.validator.addMethod("notDefaultMinutes", function (value, element) {
+        return value.trim() != "Minuter";
+    }, "Vänligen välj en tid!");
+
     $('#meetingPollForm').validate({
         rules: {
             Title: {
@@ -16,6 +24,25 @@
                 minlength: 5,
                 maxlength: 100,
                 noSpace: true
+            },
+            meetingDate: {
+                required: true
+            },
+            startHours: {
+                required: true,
+                notDefaultHour: true
+            }, 
+            startMinutes: {
+                required: true,
+                notDefaultMinutes: true
+            },
+            endHours: {
+                required: true,
+                notDefaultHour: true
+            }, 
+            endMinutes: {
+                required: true,
+                notDefaultMinutes: true
             }
         },
         messages: {
@@ -28,6 +55,21 @@
                 required: "Skriv ett meddelande!",
                 minlength: "Meddelandet måste vara minst 5 tecken!",
                 maxlength: "Meddelandet får inte vara längre än 100 tecken!"
+            }, 
+            meetingDate: {
+                required: "Du måste välja ett datum!"
+            }, 
+            startHours: {
+                required: "Du måste välja en starttid!"
+            }, 
+            startMinutes: {
+                required: "Du måste välja en starttid!"
+            }, 
+            endHours: {
+                required: "Du måste välja en sluttid!"
+            }, 
+            endMinutes: {
+                required: "Du måste välja en sluttid!"
             }
         }
     });
